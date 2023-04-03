a55608cbe496e141b84dda6d3711f9c8b479a5e0
RubTheKid <you@example.com>
2023-04-03 18:32:23 -0300

    update: post tag

---
+++ Blog.Web/Controllers/AdminTagsController.cs
@@ -0,0 +1,41 @@
+﻿using Blog.Web.Data;
+using Blog.Web.Models.Domain;
+using Blog.Web.Models.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Blog.Web.Controllers;
+                  // /AdminTags
+public class AdminTagsController : Controller
+{
+    //constructor creation + injection
+    private BlogDbContext _blogDbContext;
+    public AdminTagsController(BlogDbContext blogDbContext)
+    {
+        _blogDbContext = blogDbContext;
+    }
+
+    [HttpGet]
+              //         /Add
+    public IActionResult Add()
+    {
+        return View();
+    }
+
+    [HttpPost]
+    [ActionName("Add")]
+    public IActionResult Add(AddTagRequest addTagRequest)
+    {
+        //read input elements (map AddTagRequest to Tag domain model
+        var tag = new Tag
+        {
+            Name = addTagRequest.Name,
+            DisplayName = addTagRequest.DisplayName,
+        };
+
+        //post in db
+        _blogDbContext.Tags.Add(tag);
+        _blogDbContext.SaveChanges();
+
+        return View("Add");
+    }
+}
---
+++ Blog.Web/Models/ViewModels/AddTagRequest.cs
@@ -0,0 +1,9 @@
+﻿namespace Blog.Web.Models.ViewModels;
+
+public class AddTagRequest
+{
+    //capturar as informações inputadas no form
+
+    public string Name { get; set; }
+    public string DisplayName { get; set; }
+}
