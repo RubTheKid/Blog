fcb1feb83fe400f4678b621c77653bab24a621f8
RubTheKid <you@example.com>
2023-04-04 18:57:12 -0300

    update

--- Blog.Web/Controllers/AdminTagsController.cs
+++ Blog.Web/Controllers/AdminTagsController.cs
@@ -4,18 +4,18 @@ using Blog.Web.Models.ViewModels;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Blog.Web.Controllers;
-                  // /AdminTags
+// /AdminTags
 public class AdminTagsController : Controller
 {
     //constructor creation + injection
-    private BlogDbContext _blogDbContext;
+    private readonly BlogDbContext _blogDbContext;
     public AdminTagsController(BlogDbContext blogDbContext)
     {
         _blogDbContext = blogDbContext;
     }
 
     [HttpGet]
-              //         /Add
+    //         /Add
     public IActionResult Add()
     {
         return View();
@@ -36,6 +36,68 @@ public class AdminTagsController : Controller
         _blogDbContext.Tags.Add(tag);
         _blogDbContext.SaveChanges();
 
-        return View("Add");
+        return RedirectToAction("List");
     }
+
+    [HttpGet]
+    [ActionName("List")]
+    public IActionResult List()
+    {
+        var tags = _blogDbContext.Tags.ToList();
+
+        return View(tags);
+    }
+
+    [HttpGet]
+    [ActionName("Edit")]
+    public IActionResult Edit(Guid id)
+    {
+        var tag = _blogDbContext.Tags.FirstOrDefault(x => x.Id == id);
+
+        if (tag != null)
+        {
+            var editTagRequest = new EditTagRequest
+            {
+                Id = tag.Id,
+                Name = tag.Name,
+                DisplayName = tag.DisplayName
+            };
+            return View(editTagRequest);
+        }
+        return View(null);
+    }
+    [HttpPost]
+    public IActionResult Edit(EditTagRequest editTagRequest)
+    {
+        var tag = new Tag
+        {
+            Id = editTagRequest.Id,
+            Name = editTagRequest.Name,
+            DisplayName = editTagRequest.DisplayName,
+        };
+        var existingTag = _blogDbContext.Tags.Find(tag.Id);
+
+        if (existingTag != null)
+        {
+            existingTag.Name = tag.Name;
+            existingTag.DisplayName = tag.DisplayName;
+
+            _blogDbContext.SaveChanges();
+
+            return RedirectToAction("List");
+        }
+        return RedirectToAction("Edit", new { id = editTagRequest.Id });
+    }
+
+
+
+    [HttpDelete]
+    [ActionName("Delete")]
+    public IActionResult Delete(int id)
+    {
+
+
+        return RedirectToAction("List");
+    }
+
 }
