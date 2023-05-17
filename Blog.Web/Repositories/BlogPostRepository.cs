7691778641b479343e12457bea999b9f7dbf700b
RubTheKid <jadercardoso@tuta.io>
2023-05-17 17:27:27 -0300

    edit functionality

--- Blog.Web/Repositories/BlogPostRepository.cs
+++ Blog.Web/Repositories/BlogPostRepository.cs
@@ -31,9 +31,9 @@ public class BlogPostRepository : IBlogPostRepository
 
     }
 
-    public Task<BlogPost?> GetAsync(Guid id)
+    public async Task<BlogPost?> GetAsync(Guid id)
     {
-        throw new NotImplementedException();
+        return await blogDbContext.BlogPosts.Include(x => x.Tags).FirstOrDefaultAsync(x => x.Id == id);
     }
 
     public Task<BlogPost?> UpdateAsync(BlogPost blogPost)
