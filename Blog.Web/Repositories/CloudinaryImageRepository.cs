a79f0150281801082ac63b347dcc84be89ff3cc6
RubTheKid <jadercardoso@tuta.io>
2023-05-19 14:33:19 -0300

    cloudinary sdk + upload method

--- Blog.Web/Repositories/CloudinaryImageRepository.cs
+++ Blog.Web/Repositories/CloudinaryImageRepository.cs
@@ -1,10 +1,41 @@
-﻿namespace Blog.Web.Repositories;
+﻿using CloudinaryDotNet;
+using CloudinaryDotNet.Actions;
 
-public interface CloudinaryImageRepository : IImageRepository
+namespace Blog.Web.Repositories;
+
+public class CloudinaryImageRepository : IImageRepository
 {
-    public Task<string> UploadAsync(IFormFile file)
+    private readonly IConfiguration configuration;
+    private readonly Account account;
+
+    public CloudinaryImageRepository(IConfiguration configuration)
     {
-        throw new NotImplementedException();
+        this.configuration = configuration;
+        account = new Account(
+            configuration.GetSection("Cloudinary")["CloudName"],
+            configuration.GetSection("Cloudinary")["ApiKey"],
+            configuration.GetSection("Cloudinary")["ApiSecret"]
+            );
+
+    }
+    public async Task<string> UploadAsync(IFormFile file)
+    {
+        var client = new Cloudinary(account);
+
+        // Upload
+        var uploadParams = new ImageUploadParams()
+        {
+            File = new FileDescription(file.FileName, file.OpenReadStream()),
+            DisplayName = file.FileName
+        };
+
+        var uploadResult = await client.UploadAsync(uploadParams);
+
+        if(uploadResult != null && uploadResult.StatusCode == System.Net.HttpStatusCode.OK)
+        {
+            return uploadResult.SecureUri.ToString();
+        }
+        return null;
     }
 
 }
